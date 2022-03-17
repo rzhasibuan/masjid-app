a397c8c70816dd8a82f15cbd6ca78f97527f78d3
rzhasibuan <rzhasibuan@gmail.com>
2022-03-18 01:43:27 +0700

    update title

--- resources/views/admin/templates/partials/head.blade.php
+++ resources/views/admin/templates/partials/head.blade.php
@@ -2,7 +2,7 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="csrf-token" content="{{ csrf_token() }}">
-      <title> {{$title ?? ""}} | ENGLISH LECTURERS AND TEACHERS ASSOCIATION </title>
+      <title> {{$title ?? ""}} | Masjid Application </title>
       <!-- Tell the browser to be responsive to screen width -->
       <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
       <link rel="stylesheet" href="{{asset('assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
