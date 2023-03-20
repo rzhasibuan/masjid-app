08811437757e996a7973b21d8d1cfe1219e61362
rzhasibuan <rzhasibuan@gmail.com>
2023-03-20 11:48:33 +0700

    update header view img title

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -4,7 +4,7 @@
                 @foreach($header as $data)
                     <div class="swiper-slide">
                         <img data-src=" {{ asset('storage/')}}/{{$data->thumbnail ?? "https://images.pexels.com/photos/1824273/pexels-photo-1824273.jpeg?cs=srgb&dl=pexels-victor-miyata-1824273.jpg&fm=jpg"}}"
-                             class="swiper-lazy"/>
+                             class="swiper-lazy" alt="{{$data->text}}, {{$data->bigtitle}}"/>
                     </div>
                 @endforeach
             </div>
